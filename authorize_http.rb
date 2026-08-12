ea0dff368c90eea5f5bb54fc647a91e8f8d8468d
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-08-11 23:12:19 -0300

    fix: return boolean from all paths in AuthorizeHttp

--- authorize_http.rb
+++ authorize_http.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'json'
+require 'net/http'
+require 'uri'
+
 class AuthorizeHttp
   
   URL = URI('https://util.devi.tools/api/v2/authorize')
@@ -7,14 +11,19 @@ class AuthorizeHttp
 
   def autorizado?
 
-    response = Net::HTTP.get(URL)
+    response = Net::HTTP.get_response(URL)
 
-        if response.code == '200'
-          return JSON.parse(response.body).dig('data', 'authorization') == true
-        end
-      rescue OpenSSL::SSL::SSLError, SocketError, JSON::ParserError => e
-        puts "Não foi possível consultar a autorização: #{e.class}"
-        false
-      end
-  
+          if response.code == '200'
+            return JSON.parse(response.body).dig('data', 'authorization') == true 
+          end 
+
+          if response.code != '200' 
+            return false
+          end
+
+    rescue OpenSSL::SSL::SSLError, SocketError, JSON::ParserError => e
+    puts "Não foi possível consultar a autorização: #{e.class}"
+    false
   end
+    
+end
