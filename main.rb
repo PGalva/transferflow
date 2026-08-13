681c72a76e4acf6eaf9d15a2150777b6285060cf
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-08-12 22:56:06 -0300

    fix: correct transfer method

--- main.rb
+++ main.rb
@@ -4,4 +4,9 @@ require_relative 'conta'
 require_relative 'notificationservice'
 require_relative 'saldo'
 require_relative 'lojista'
+require_relative 'authorizeFake'
+require_relative 'usuario'
 
+
+
+joao = Usuario.create("João", "123.456.789-00", "joao@email.com", "senha123", 1000.0)
--- usuario.rb
+++ usuario.rb
@@ -1,3 +1,30 @@
+require_relative 'conta'
+require_relative 'notificationservice'
+require 'json'
+require 'net/http'
+require 'authorizeFake'
+require 'AuthorizeHttp'
+
 class Usuario < Conta
+  
+  def transferir(autoriza, valor, destinatario)
+      
+    unless autoriza.autorizado?
+      puts "Transferência não autorizada."
+      return 
+    end
+    
+    if saldo >= valor
+          @saldo -= valor
+          destinatario.receber(valor)
+          puts "Transferência de R$#{valor} realizada com sucesso para #{destinatario.nome}."
+      else
+        puts "Saldo insuficiente para realizar a transferência."
+      end
+    
+
+  end
+
+
 
 end
