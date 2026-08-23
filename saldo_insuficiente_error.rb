95d1a13cc3a40b75fe5734d0f26411d1df3280ae
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-08-23 03:29:31 -0300

    fix: raise saldo errors

---
+++ saldo_insuficiente_error.rb
@@ -0,0 +1,3 @@
+class SaldoInsuficienteError < StandardError
+  
+end
--- usuario.rb
+++ usuario.rb
@@ -7,19 +7,22 @@ class Usuario < Conta
   
   def transferir(autoriza, valor, destinatario)
 
-      if valor <= 0
+    cents = to_centavos(valor)
+
+      if cents <= 0
         puts "Valor de transferência inválido. Deve ser maior que zero."
         return
       end
-
-      return puts "Saldo insuficiente." if valor > saldo 
+      
+      if cents > saldo 
+      raise "Saldo insuficiente." 
       
     unless autoriza.autorizado?
       puts "Transferência não autorizada."
       return 
     end
-         @saldo -= valor
-          destinatario.receber(valor)
+         @saldo -= cents
+          destinatario.receber(cents)
           puts "Transferência de R$#{valor} realizada com sucesso para #{destinatario.nome}."
         
     
