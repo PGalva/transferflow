c52c993112ce71f402a84970c7b98d84c600a17e
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-08-24 21:25:03 -0300

    fix:raise LojistaTransferError on transfer attempt

--- lojista.rb
+++ lojista.rb
@@ -1,17 +1,15 @@
 #!/usr/bin/env ruby
 require_relative 'conta'
 require_relative 'notificationservice'
-require_relative 'authorize_fake'
 require_relative 'authorize_http'
 require_relative 'usuario'
 
 class Lojista < Conta
 
 
-  def lojista_transferencia(autoriza, valor, destinatario)
-    if autoriza.autorizado?
+  def lojista_transferencia(*)
       raise LojistaTransferError, "Sem permissao para transferencia"
     end
 
-  end
+
 end
