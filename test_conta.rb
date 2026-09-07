ff4ad3d641d7189f2875a11552bf897c3ea60be0
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-09-07 02:27:58 -0300

    test: add first Minitest coverage for create Conta

--- test_conta.rb
+++ test_conta.rb
@@ -2,6 +2,7 @@
 
 require_relative 'conta'
 require 'minitest/autorun'
+require_relative 'usuario'
 
 
 class TestConta < Minitest::Test 
@@ -13,5 +14,15 @@ cents1 = cents.send(:to_centavos, 10.0)
 assert_equal 1000, cents1
 
 end
+
+  def test_create
+  create1 = Conta.create("A", "123.456.789-00", "a@x.com", "s", 100)
+  create2 = Conta.create("B", "12345678900", "b@x.com", "s", 100)
+
+  assert_nil create2
+
+  end
+
+  def test_transferir
 
 end
